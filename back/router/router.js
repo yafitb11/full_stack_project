8e3a20675350dfab01e72cb89615baf57902a092
yafitb11 <yafitb11@gmail.com>
2025-10-26 01:23:13 +0300

    6

--- back/router/router.js
+++ back/router/router.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const productController = require("../products/routes/productController");
 const userController = require("../users/routes/userController");
 const orderController = require("../orders/routes/orderController");
-const categoryController = require("../categories/routes/caegoryController");
+const categoryController = require("../categories/routes/categoryController");
 const { errorhandler } = require("../utils/errorhandler");
+
 
 router.use("/products", productController);
 router.use("/users", userController);
--- back/server.js
+++ back/server.js
@@ -1,7 +1,6 @@
 const { errorhandler } = require("./utils/errorhandler");
 const express = require("express");
 const app = express();
-const path = require('path');
 const chalk = require("chalk");
 const router = require("./router/router");
 const cors = require("./middlewares/cors");
@@ -15,17 +14,7 @@ app.use(cors);
 app.use(logger);
 app.use(express.json());
 app.use(express.text());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(router);
-
-app.use((req, res, next) => {
-    const filePath = path.join(__dirname, "public", "pageNotFound.html");
-    res.status(404).sendFile(filePath, (err) => {
-        if (err) {
-            next(err);
-        }
-    });
-});
 
 app.use((err, req, res, next) => {
     console.log("Error middleware triggered with error:", err);
