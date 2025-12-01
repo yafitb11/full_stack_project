0723d79a6d588585bd2aa13ee49e1a4707545ebc
yafitb11 <yafitb11@gmail.com>
2025-12-01 15:26:24 +0200

    login css

--- front/src/components/PleaseLogin.tsx
+++ front/src/components/PleaseLogin.tsx
@@ -20,7 +20,7 @@ const PleaseLogin = ({ from }: PleaseLoginProps) => {
                     You are not logged in, please log in to view this page.
                 </p>
                 <Link to="/signin" state={{ from: redirectTo }}>
-                    <Button color="blue">Login</Button>
+                    <Button color="blue" className="m-auto w-[180px] mt-16 dark:!bg-slate-800 dark:text-slate-200 dark:!border-white ">Login</Button>
                 </Link>
             </div>
         </div>
