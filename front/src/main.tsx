edbb7def70537ffc41491cc31f5a9e786f5dc53f
yafitb11 <yafitb11@gmail.com>
2025-12-02 19:02:53 +0200

    split css

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index5.css";
+import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,7 @@ import flowbite from "flowbite-react/tailwind";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}", flowbite.content()],
   theme: {
     screens: {
       xs: { max: "639px" },
